8797663197a61ca08ef97c0d3e5f6a510afbcdd2
looiekang <lnk18@scarletmail.rutgers.edu>
2016-12-14 17:33:11 -0500

    enemies drop power up

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -48,9 +48,13 @@ public class Weapon : MonoBehaviour
     public GameObject collar;
     public float lastShot; // Time last shot was fired
 
-    void Start()
+    void Awake()
     {
         collar = transform.Find("Collar").gameObject;
+    }
+    
+
+    void Start() {
         // Call SetType() properly for the default _type
         SetType(_type);
 
